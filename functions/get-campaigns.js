58aa311110f08818adf914f1fd0bb02cc1db4d26
virc <icbrvdr@gmail.com>
2021-03-17 13:42:44 +0530

    update lambda to asyc-await

--- functions/get-campaigns.js
+++ functions/get-campaigns.js
@@ -8,26 +8,24 @@ const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
 });
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event, context) {
 
-  client
-    .query(
-      q.Map(
-        q.Paginate(q.Documents(q.Collection("campaigns"))),
-        q.Lambda((post) => q.Get(post))
-      )
-    )
-    .then((res) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          status: "OK",
-          data: res
-        }),
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const res = await client.query(
+        q.Map(q.Paginate(q.Documents(q.Collection("campaigns"))), q.Lambda((post) => q.Get(post)))
+    );
 
+  let campaigns = [];
+  if(res.data && Array.isArray(res.data)){
+    campaigns = res.data.map((item)=>item.data)
+  }
+
+  return {
+      statusCode: 200,
+      body: JSON.stringify({
+        status: "OK",
+        data: campaigns
+      }),
+    }
 };
+
+(async ()=> console.log( await exports.handler({},{})))()
