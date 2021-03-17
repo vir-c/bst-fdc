1aa4e61d0d29cb0d3ef25430dc3a64f556b22c83
virc <icbrvdr@gmail.com>
2021-03-17 12:43:21 +0530

    add lerna anfd lambda function

---
+++ functions/get-campaigns.js
@@ -0,0 +1,33 @@
+/*lambda function to fetch campaign list from faunadb */
+
+var faunadb = require("faunadb"),
+  q = faunadb.query;
+
+/* configure faunaDB Client with secret key*/
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+});
+
+exports.handler = function (event, context, callback) {
+
+  client
+    .query(
+      q.Map(
+        q.Documents(q.Collection("campaigns")),
+        q.Lambda((post) => q.Get(post))
+      )
+    )
+    .then((res) => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: "OK",
+          data: res
+        }),
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+};
